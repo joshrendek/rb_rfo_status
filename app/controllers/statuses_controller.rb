d79d07473a72fe80e692223c2d4ee2e1f5420584
Josh Rendek <josh@bluescripts.net>
2012-12-02 22:52:03 -0500

    more styling

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -14,6 +14,7 @@ class StatusesController < ApplicationController
   # GET /statuses/1.json
   def show
     @status = Status.find(params[:id])
+    @status_update = StatusUpdate.new
 
     respond_to do |format|
       format.html # show.html.erb
