de76841a958f6acf4405b6ef0e83714077ec7e48
Josh Rendek <josh@bluescripts.net>
2012-12-02 22:28:43 -0500

    style updates and some other small tweaks

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,6 +1,10 @@
 class Status < ActiveRecord::Base
-  attr_accessible :closed, :duration, :environment_affected, :title
+  attr_accessible :closed, :duration, :environment_affected, :title, :status_updates_attributes
   has_many :status_updates, :dependent => :destroy 
 
   accepts_nested_attributes_for :status_updates
+
+
+  scope :production, where(environment_affected: 'Production')
+  scope :development, where(environment_affected: 'Development')
 end
--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -1,4 +1,6 @@
 class StatusUpdate < ActiveRecord::Base
   belongs_to :status
   attr_accessible :content, :state, :status_id
+
+  default_scope order('id desc')
 end
